c7f962212bd80c481c145b3540aeed74e41f7cb4
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-04-18 11:04:17 +0100

    added new files and assets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/img/Logo.svg'
+
+function Navbar() {
+  return (
+    <nav>
+        <img alt='logo' src={logo}/>
+    </nav>
+  )
+}
+
+export default Navbar
