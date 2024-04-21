0358ab0e1dd6a1dfe2289ec76516f903a80957ed
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-04-21 03:09:59 +0100

    completed footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { useState } from 'react';
-import { Navbar, Hero, Specials, Testimonials, About } from './components';
+import { Navbar, Hero, Specials, Testimonials, About, Footer, Login } from './components';
 
 function App() {
 
   const [navBg, setNavBg] = useState('nav-transparent')
+  const [login, setlogin] = useState(true)
 
   const setNavStyle = () => {
     // USING THE useEffect Hook:
@@ -12,17 +13,25 @@ function App() {
     // based on Y scroll direction
   }
 
+  const triggerLogin = (prev) => {
+    setlogin(prev == true ? false : true)
+  }
+
   return (
     <>
       <header className={navBg}>
-        <Navbar />
+        <Navbar setlogin={triggerLogin}/>
       </header>
       <main>
+        {login && <Login setlogin={triggerLogin} />}
         <Hero />
         <Specials />
         <Testimonials />
         <About />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import {footerLinks} from '../constants'
+import restaurant from '../assets/img/restauranfood.jpg'
+
+function Footer() {
+  return (
+    <section className="footer-section">
+        <div className='footer-content' >
+            <div className="footer-imgbx">
+                <img className='footer-img' src={restaurant} alt="logo" />
+            </div>
+            <div className="footerlink-container">
+            {
+                footerLinks.map((content) => {
+                return(
+                    <div key={content.title} className="footerlink-bx">
+                    <p className="footlink-head">
+                        {content.title}
+                    </p>
+                    <div>
+                        {
+                        content.links.map((item, index) => {
+                            return(
+                            <a key={item.name} href={item.link} target="">
+                                <p className={`footlink-text`}>{item.name}</p>
+                            </a>
+                            )
+                        })
+                        }
+                    </div>
+                    </div>
+                )
+                })
+            }
+            </div>
+        </div>
+        {/* <div className="">
+        <p className="">Copyright ©️  2024 JoeBank. All Rights Reserved.</p>
+            <div className="">
+                {
+                socialMedia.map((socials, index) => {
+                    return(
+                    <a href={socials.link} key={socials.id} target="_blank"
+                    className={`${index != socialMedia.length - 1 ? "mr-6" : "mr-0"}`} >
+                        <img className="" src={socials.icon}  />
+                    </a>
+                    )
+                })
+                }
+            </div>
+        </div> */}
+    </section>
+  )
+}
+
+export default Footer
