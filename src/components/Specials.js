0aeee52b7a6db4f073cd13cdab9fd1c96c41c5aa
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-07-02 09:06:23 +0100

    updated specials card to carousel

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import Button from './Button'
 import Card from './Card'
 import { cardInfo } from '../constants'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css';
+import GreekSalad from '../assets/img/greek salad.jpg'
 
 function Specials({toggleIconClose}) {
 
@@ -32,6 +35,22 @@ function Specials({toggleIconClose}) {
     },
     [window.innerWidth])
 
+    // 
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1100 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1100, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 640, min: 0 },
+          items: 1
+        }
+      };
+
     // create two arrays:
     // Duplicate card in array1
     // Array 2 would contain starting index of the content of array 1,
@@ -47,17 +66,23 @@ function Specials({toggleIconClose}) {
                     <h3>Specials</h3>
                     <Button text={"Online Menu"} />
                 </div>
-                <div className='specials-body'>
-                    <div className='card-container'>
-                        {
-                            cardInfo.map((card, index) => {
-                                return(
-                                    <Card key={index} img={card.img} meal={card.meal}
-                                    price={card.price} details={card.details} />
-                                )
-                            })
-                        }
-                    </div>
+                <div className='specials-carousel'>
+                    <Carousel responsive={responsive}
+                    infinite={true}
+                    autoPlay={true}
+                    autoPlaySpeed={1500}
+                    containerClass="carousel-container"
+                    removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
+                    >
+                    {
+                        cardInfo.map((card, index) => {
+                            return(
+                                <Card key={index} img={card.img} meal={card.meal}
+                                price={card.price} details={card.details} />
+                            )
+                        })
+                    }
+                    </Carousel>
                 </div>
                 <div className='smobile-btn'>
                     <Button text={"Online Menu"} />
